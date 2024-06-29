2398b5743f20d967ad9db55e533cdd01736abde8
xyy90 <xyy010890@outlook.com>
2024-06-29 15:44:30 +0800

    测试

--- api_ceshi.py
+++ api_ceshi.py
@@ -3,5 +3,11 @@ from kubernetes import client,config
 class Kube(object):
     api_instance=client.CoreV1Api()
     api_apps=client.AppsV1Api()
-    def __init__(self) -> None:
-        pass
+    def __init__(self,conf=None) -> None:
+        if conf:
+            config.load_kube_config(config_file=conf)
+        else:
+            config.load_incluster_config()
+    def change_env(self,name,namespace):
+        result=self.api_apps.read_namespaced_stateful_set(name=name,namespace=namespace)
+        print(result)
